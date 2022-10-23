cf1bac443bda0907c9fab42d9cbd283ccfda22ce
Monisha <monisha13251@gmail.com>
2022-10-23 21:03:17 +0530

    try test

--- test_main.py
+++ test_main.py
@@ -38,3 +38,8 @@ def test_auth(client):
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
+
+def test_fail(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json == 'notHealthy'
